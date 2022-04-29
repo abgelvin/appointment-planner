1b838b6a26404706d6075b04aa0386de62af3f46
60162973+abgelvin@users.noreply.github.com <abgelvin@gmail.com>
2022-04-29 11:53:33 -0600

    finished steps

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,40 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input 
+          type='text' 
+          name='name' 
+          value={name} 
+          onChange={(e) => setName(e.target.value)} 
+          required 
+          placeholder="Contact Name">
+        </input>
+      </label>
+      <label>
+        <input 
+          type='tel' 
+          name='phone'
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          required
+          pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4} "
+          placeholder="Contact Phone (###-###-####)" >
+        </input>
+      </label>
+      <label>
+        <input
+          type='email' 
+          name='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          placeholder="Contact Email" >
+        </input>
+      </label>
+      <br />
+      <label type='submit' value='Add Contact' ></label>
+    </form>
   );
 };
--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
+import { TileList } from "../../components/tileList/TileList";
+import { AppointmentForm } from '../../components/appointmentForm/AppointmentForm'
 
-export const AppointmentsPage = () => {
+export const AppointmentsPage = ({appointments, addAppointment, contacts}) => {
+  const [title, setTitle] = useState('')
+  const [contact, setContact] = useState(
+    contacts.length > 0 ? contacts[0].name : ''
+  )
+  const [date, setDate] = useState('')
+  const [time, setTime] = useState('')
+  
   /*
   Define state variables for 
   appointment info
@@ -8,20 +17,33 @@ export const AppointmentsPage = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data  
-    */
-   
+    addAppointment(title, contact, date, time)
+    setTitle('')
+    setContact('')
+    setDate('')
+    setTime('')
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm
+          contacts={contacts} 
+          title={title} 
+          contact={contact} 
+          date={date} 
+          time={time} 
+          setTitle={setTitle} 
+          setContact={setContact} 
+          setDate={setDate}
+          setTime={setTime}
+          handleSubmit={handleSubmit} />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList tiles={appointments} />
       </section>
     </div>
   );
